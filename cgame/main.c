4166057f8a310cb52bc4ca5d825ccf4746bcbf08
junhaddi <rkdwnsgk05@gmail.com>
2018-11-01 17:46:43 +0900

    Add framework

--- cgame/main.c
+++ cgame/main.c
@@ -1,14 +1,65 @@
+#define LEFT 75
+#define RIGHT 77
+#define UP 72
+#define DOWN 80
 
+#define BLUE 9
+#define GREEN 10
+#define SKY 11
+#define RED 12
+#define PURPLE 13
+#define YELLOW 14
+#define WHITE 15
+
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>	UTIL
+int key;
+
+void gotoxy(int x, int y) {
+	COORD Pos;
+	Pos.X = x;
+	Pos.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+}
+
+void setColor(unsigned short text)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), text);
+}
+
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>	GAME
 void init() {
-
+	setColor(BLUE);
 }
 
 void update() {
-
+	if (kbhit()) {
+		key = getch();
+		switch (key) {
+			case LEFT:
+				printf("LEFT");
+				break;
+			case RIGHT:
+				printf("RIGHT");
+				break;
+			case UP:
+				printf("UP");
+				break;
+			case DOWN:
+				printf("DOWN");
+				break;
+		}
+	}
 }
 
 void render() {
-
+	system("cls");
 }
 
 void free() {
@@ -16,6 +67,13 @@ void free() {
 }
 
 int main() {
+	init();
+
+	for (;;) {
+		update();
+		render();
+	}
+	free();
 
 	return 0;
 }

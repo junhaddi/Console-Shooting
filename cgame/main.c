8e444bf6ebc07058044db9f120227a3669806c94
junhaddi <rkdwnsgk05@gmail.com>
2018-11-04 06:38:14 +0900

    v1.0

--- cgame/main.c
+++ cgame/main.c
@@ -82,8 +82,6 @@ void setcursortype(CURSOR_TYPE c) {
 //	SYS
 BOOL isGameOver = FALSE;
 int score = 0;
-int bossHpMax = 100;
-int bossHp = 100;
 
 //	PLAYER
 int px = GAME_WIDTH / 2;
@@ -99,6 +97,13 @@ struct {
 int bx = GAME_WIDTH / 2 - 3;
 int bw = 7;
 int by = 5;
+int bossHpMax = 100;
+int bossHp = 100;
+
+//	ITEM
+BOOL isItemExist;
+int ix;
+int iy;
 
 //	ENEMY NORMAL
 struct {
@@ -106,21 +111,36 @@ struct {
 	int x, y, dir;
 } Enemy[MAXENEMY];
 
-void colBoss();
-void colEnemy();
+//	PROTOTYPE
+void logo();
+
 void colPlayer();
-void bossRender();
+void colEnemy();
+void colBoss();
+void colItem();
+
+void itemGen();
+void itemMove();
+void itemClear();
+
 void enemyClear(int);
 void enemyGen();
 void enemyMove();
+
 void bulletClear(int);
 void bulletRender(int);
 void bulletMove();
+
 void playerClear();
 void playerRender();
 void playerAction();
-void drawBossHp();
+
+void bossRender();
 void ui();
+void drawBossHp();
+void drawScore();
+
+void gameOver();
 #pragma endregion
 
 #pragma region MAIN
@@ -130,146 +150,126 @@ void ui();
 void init() {
 	setcursortype(NOCURSOR);
 	system("mode con:cols=80 lines=40");
+	srand(time(NULL));
+
+	logo();
 	ui();
 	drawBossHp();
-	srand(time(NULL));
 }
 
 void update() {
 	playerAction();
+
 	bulletMove();
+
 	enemyMove();
 	enemyGen();
-	colBoss();
-	colEnemy();
+
+	itemMove();
+	itemGen();
+
 	colPlayer();
+	colItem();
+	colEnemy();
+	colBoss();
 }
 
 void render() {
 	playerRender();
 	bossRender();
+	drawScore();
 }
 
 int main() {
 	init();
 	for (;;) {
-		update();
-		render();
-		Sleep(30);
+		if (isGameOver == FALSE) {
+			update();
+			render();
+			Sleep(30);
+		}
+		else {
+			break;
+		}
 	}
-	getch();
+	gameOver();
 	return 0;
 }
 #pragma endregion
 
 #pragma region ENGINE
-//	COLISION
-void colBoss() {
-	for (int i = 0; i < MAXBULLET; i++) {
-		if (Bullet[i].isExist == FALSE) {
-			continue;
-		}
-		if (Bullet[i].x >= bx && Bullet[i].x <= bx + bw && Bullet[i].y <= by) {
-			//	HIT
-			bulletClear(i);
-			Bullet[i].isExist = FALSE;
-			bossHp -= 10;
-			drawBossHp();
-		}
-	}
+//	MENU
+void logo() {
+	setColor(WHITE);
+	gotoxy(GAME_WIDTH / 2 - 3, GAME_HEIGHT / 2);
+	printf("R A I D");
+	gotoxy(GAME_WIDTH / 2 - 8, GAME_HEIGHT / 2 + 2);
+	printf("~ Press Any Key ~");
+	gotoxy(GAME_WIDTH / 2 + 20, GAME_HEIGHT - 2);
+	printf("-2018.11.04 KJH");
+	getch();
+	system("cls");
 }
 
-void colEnemy() {
-	int i;
-	for (i = 0; i < MAXENEMY; i++) {
-		if (Enemy[i].isExist == FALSE) {
-			continue;
-		}
-		for (int j = 0; j < MAXBULLET; j++) {
-			if (Bullet[j].isExist == FALSE) {
-				continue;
-			}
-			if (Enemy[i].x == Bullet[j].x && Enemy[i].y == Bullet[j].y) {
-				enemyClear(i);
-				Enemy[i].isExist = FALSE;
-				bulletClear(j);
-				Bullet[j].isExist = FALSE;
-			}
-		}
-	}
+void gameOver() {
+	system("cls");
+	setColor(WHITE);
+	gotoxy(35, 18);
+	printf("GAMEOVER", score);
+	gotoxy(35, 20);
+	printf("YOUR SCORE: %d", score);
+	Sleep(3000);
+	exit(0);
 }
 
-void colPlayer() {
-	for (int i = 0; i < MAXBULLET; i++) {
-		if (Enemy[i].isExist == FALSE) {
-			continue;
-		}
-		if (px == Enemy[i].x && py == Enemy[i].y) {
-			exit(0);
-		}
-	}
-}
-
-// BOSS
-void bossRender() {
-	gotoxy(bx, 2);
-	printf("|||||||");
-	gotoxy(bx, 3);
-	printf("OOOOOOO");
-	gotoxy(bx, 4);
-	printf("O     O");
-	gotoxy(bx, by);
-	printf("OOOOOOO");
-}
-
-//	ENEMY
-void enemyClear(int i) {
-	gotoxy(Enemy[i].x, Enemy[i].y);
+//	PLAYER
+void playerClear() {
+	gotoxy(px, py);
 	printf(" ");
 }
 
-void enemyGen() {
-	int i;
-	if (rand() % 20 == 0) {
-		for (i = 0; i < MAXENEMY && Enemy[i].isExist == TRUE; i++) { ;; }
-		if (i != MAXENEMY) {
-			if (rand() % 2 == 0) {
-				Enemy[i].x = 1;
-				Enemy[i].y = rand() % (GAME_HEIGHT - 1);
-				Enemy[i].dir = 1;
-			}
-			else {
-				Enemy[i].x = GAME_WIDTH - 1;
-				Enemy[i].y = rand() % (GAME_HEIGHT - 1);
-				Enemy[i].dir = -1;
-			}
-		}
-		Enemy[i].isExist = TRUE;
-	}
+void playerRender() {
+	setColor(YELLOW);
+	gotoxy(px, py);
+	printf("@");
 }
 
-void enemyMove() {
-	int i, move = 0;
-	if (rand() % 5 == 0) {
-		move = 1;
+void playerAction() {
+	if (GetAsyncKeyState(VK_LEFT) < 0 && px > 0) {
+		playerClear();
+		px--;
+		playerRender();
 	}
-	for (i = 0; i < MAXENEMY; i++) {
-		if (Enemy[i].isExist == FALSE) {
-			continue;
-		}
-		if (Enemy[i].x < 1 || Enemy[i].x > GAME_WIDTH - 1) {
-			//	DESTROY
-			Enemy[i].isExist = FALSE;
-			enemyClear(i);
-			score++;
-		}
-		else {
-			//	MOVE
-			enemyClear(i);
-			Enemy[i].x += Enemy[i].dir * move;
-			setColor(RED);
-			gotoxy(Enemy[i].x, Enemy[i].y);
-			printf("M");
+	if (GetAsyncKeyState(VK_RIGHT) < 0 && px < GAME_WIDTH) {
+		playerClear();
+		px++;
+		playerRender();
+	}
+	if (GetAsyncKeyState(VK_UP) < 0 && py > by + 1) {
+		playerClear();
+		py--;
+		playerRender();
+	}
+	if (GetAsyncKeyState(VK_DOWN) < 0 && py < GAME_HEIGHT - 1) {
+		playerClear();
+		py++;
+		playerRender();
+	}
+
+	if (kbhit()) {
+		int ch = getch();
+		switch (ch) {
+			int i;
+		case ' ':
+			for (i = 0; i < MAXBULLET && Bullet[i].isExist == TRUE; i++) { ;; }
+			if (i != MAXBULLET) {
+				Bullet[i].x = px;
+				Bullet[i].y = py;
+				Bullet[i].isExist = TRUE;
+			}
+			break;
+		case ESC:
+			exit(0);
 		}
 	}
 }
@@ -301,54 +301,165 @@ void bulletMove() {
 	}
 }
 
-//	PLAYER
-void playerClear() {
-	gotoxy(px, py);
+//	ENEMY
+void enemyClear(int i) {
+	gotoxy(Enemy[i].x, Enemy[i].y);
 	printf(" ");
 }
 
-void playerRender() {
-	setColor(YELLOW);
-	gotoxy(px, py);
-	printf("@");
+void enemyGen() {
+	int i;
+	if (rand() % 20 == 0) {
+		for (i = 0; i < MAXENEMY && Enemy[i].isExist == TRUE; i++) { ;; }
+		if (i != MAXENEMY) {
+			if (rand() % 2 == 0) {
+				Enemy[i].x = 1;
+				Enemy[i].y = rand() % (GAME_HEIGHT - by - 1) + by + 1;
+				Enemy[i].dir = 1;
+			}
+			else {
+				Enemy[i].x = GAME_WIDTH - 1;
+				Enemy[i].y = rand() % (GAME_HEIGHT - by - 1) + by + 1;
+				Enemy[i].dir = -1;
+			}
+		}
+		Enemy[i].isExist = TRUE;
+	}
 }
 
-void playerAction() {
-	if (GetAsyncKeyState(VK_LEFT) < 0 && px > 0) {
-		playerClear();
-		px--;
-		playerRender();
+void enemyMove() {
+	int i, move = 0;
+	if (rand() % 2 == 0) {
+		move = 1;
 	}
-	if (GetAsyncKeyState(VK_RIGHT) < 0 && px < GAME_WIDTH) {
-		playerClear();
-		px++;
-		playerRender();
+	for (i = 0; i < MAXENEMY; i++) {
+		if (Enemy[i].isExist == FALSE) {
+			continue;
+		}
+		if (Enemy[i].x < 1 || Enemy[i].x > GAME_WIDTH - 1) {
+			//	DESTROY
+			Enemy[i].isExist = FALSE;
+			enemyClear(i);
+			score++;
+		}
+		else {
+			//	MOVE
+			enemyClear(i);
+			Enemy[i].x += Enemy[i].dir * move;
+			setColor(RED);
+			gotoxy(Enemy[i].x, Enemy[i].y);
+			printf("M");
+		}
 	}
-	if (GetAsyncKeyState(VK_UP) < 0 && py > 0) {
-		playerClear();
-		py--;
-		playerRender();
-	}
-	if (GetAsyncKeyState(VK_DOWN) < 0 && py < GAME_HEIGHT - 1) {
-		playerClear();
-		py++;
-		playerRender();
-	}
+}
 
-	if (kbhit()) {
-		int ch = getch();
-		switch (ch) {
-			int i;
-		case ' ':
-			for (i = 0; i < MAXBULLET && Bullet[i].isExist == TRUE; i++) { ;; }
-			if (i != MAXBULLET) {
-				Bullet[i].x = px;
-				Bullet[i].y = py;
-				Bullet[i].isExist = TRUE;
+// BOSS
+void bossRender() {
+	gotoxy(bx, 2);
+	printf("|||||||");
+	gotoxy(bx, 3);
+	printf("OOOOOOO");
+	gotoxy(bx, 4);
+	printf("O     O");
+	gotoxy(bx, by);
+	printf("OOOOOOO");
+}
+
+//	ITEM
+void itemGen() {
+	if (isItemExist == FALSE) {
+		if (rand() % 100 == 0) {
+			ix = rand() % (GAME_WIDTH - 1);
+			iy = rand() % by + 1;
+			isItemExist = TRUE;
+		}
+	}
+}
+
+void itemMove() {
+	int move = 0;
+	if (rand() % 1 == 0) {
+		move = 1;
+	}
+	if (isItemExist) {
+		if (iy >= GAME_HEIGHT - 1) {
+			//	DESTROY
+			isItemExist = FALSE;
+			itemClear();
+		}
+		else {
+			//	MOVE
+			itemClear();
+			iy += move;
+			setColor(YELLOW);
+			gotoxy(ix, iy);
+			printf("P");
+		}
+	}
+}
+
+void itemClear() {
+	gotoxy(ix, iy);
+	printf(" ");
+}
+
+//	COLLISION
+void colBoss() {
+	for (int i = 0; i < MAXBULLET; i++) {
+		if (Bullet[i].isExist == FALSE) {
+			continue;
+		}
+		if (Bullet[i].x >= bx && Bullet[i].x <= bx + bw && Bullet[i].y <= by) {
+			//	HIT
+			bulletClear(i);
+			Bullet[i].isExist = FALSE;
+			bossHp -= 1;
+			if (bossHp <= 0) {
+				isGameOver = TRUE;
 			}
-			break;
-		case ESC:
-			exit(0);
+			drawBossHp();
+		}
+	}
+}
+
+void colItem() {
+	if (ix == px && iy == py) {
+		score += 10;
+		itemClear();
+		isItemExist = FALSE;
+	}
+}
+
+void colEnemy() {
+	int i;
+	for (i = 0; i < MAXENEMY; i++) {
+		if (Enemy[i].isExist == FALSE) {
+			continue;
+		}
+		for (int j = 0; j < MAXBULLET; j++) {
+			if (Bullet[j].isExist == FALSE) {
+				continue;
+			}
+			if (Enemy[i].x == Bullet[j].x && Enemy[i].y == Bullet[j].y) {
+				enemyClear(i);
+				Enemy[i].isExist = FALSE;
+				bulletClear(j);
+				Bullet[j].isExist = FALSE;
+				score++;
+			}
+		}
+	}
+}
+
+void colPlayer() {
+	for (int i = 0; i < MAXBULLET; i++) {
+		if (Enemy[i].isExist == FALSE) {
+			continue;
+		}
+		if (px == Enemy[i].x && py == Enemy[i].y) {
+			//	DIE
+			itemClear();
+			isGameOver = TRUE;
 		}
 	}
 }
@@ -371,6 +482,11 @@ void drawBossHp() {
 			printf("#");
 		}
 	}
+}
+
+void drawScore() {
+	gotoxy(2, 2);
+	printf("SOCRE: %d", score);
 }
 
 void ui() {
